612601d2629168f6a016fd2a68a3d0ab4677da8a
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-07-07 13:37:56 +0300

    goit-js-hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', onFormSubmit);
 
 let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
 const { email, message } = form.elements;
- reloadPage()
+reloadPage();
 
 function onInputData(e) {
   dataForm = { email: email.value, message: message.value };
